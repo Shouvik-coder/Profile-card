7c820851ff84d006c0afad8d7417a71d8f3f93b0
unknown <shouvik72500@gmail.com>
2024-02-11 16:31:16 +0530

    Added profile card with skill addition functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+// const skills = [
+// ];
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [isOpen, setIsOpen] = useState(true);
+	return (
+		<div className="card">
+			{isOpen && <Avatar />}
+			<div className="data">
+				<Intro />
+				<SkillList />
+				<br />
+				<button
+					style={{
+						backgroundColor: isOpen ? 'red' : 'green',
+					}}
+					className="skill"
+					onClick={() => {
+						setIsOpen((s) => !s);
+					}}
+				>
+					<b>{isOpen ? 'Hide🔒' : 'Show🔓'}</b>
+				</button>
+			</div>
+		</div>
+	);
+}
+
+function Avatar() {
+	return (
+		<img
+			className="avatar"
+			src="https://media.licdn.com/dms/image/D4D03AQF9jFNXiYbkCg/profile-displayphoto-shrink_800_800/0/1689098707154?e=2147483647&v=beta&t=B-EeAtk6UxIrlEdZMD7b6i3P2FZvVpkLmgdPuaIkmHg"
+		/>
+	);
+}
+
+function Intro() {
+	return (
+		<div>
+			<h1>Shouvik Kumar Chakraborty</h1>
+			<p>Developer aspiring to be a good one</p>
+		</div>
+	);
+}
+
+function SkillList() {
+	const [skills, setSkills] = useState(['HTML', 'CSS', 'JS']);
+	let [textValue, setTextValue] = useState('');
+	return (
+		<>
+			<input value={textValue} type="text" onChange={(e) => setTextValue(e.target.value)} />
+			<br />
+			<br />
+			<button
+				className="skill"
+				style={{
+					backgroundColor: 'lightgreen',
+				}}
+				onClick={() => {
+					setSkills((s) => [...s, textValue]);
+					setTextValue('');
+				}}
+			>
+				<b>ADD SKILL</b>
+			</button>
+			<div className="skill-list">
+				{skills.map((skill) => (
+					<Skill skillName={skill} skillColor="yellow" key={skill} />
+				))}
+			</div>
+		</>
+	);
+}
+
+function Skill(props) {
+	return (
+		<div className="skill" style={{ backgroundColor: props.skillColor }}>
+			{props.skillName}
+		</div>
+	);
 }
 
 export default App;
